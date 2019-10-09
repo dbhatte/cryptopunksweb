7ce1381d8d90807edeeb78a86fb1606883ee3bc6
Devendra <devendra.bhatte@raisin.com>
2019-10-09 07:08:32 +0200

    Added Spring Boot Starter for Web

---
+++ src/test/java/com/devendra/cryptopunks/CryptopunksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devendra.cryptopunks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CryptopunksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
