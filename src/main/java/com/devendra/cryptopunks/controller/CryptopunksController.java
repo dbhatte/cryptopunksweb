76bede75d3ad7b7d67910d3a851608ae07add9c0
Devendra <devendra.bhatte@raisin.com>
2019-10-11 07:55:09 +0200

    fixed bug for missing Cryptopunks

--- src/main/java/com/devendra/cryptopunks/controller/CryptopunksController.java
+++ src/main/java/com/devendra/cryptopunks/controller/CryptopunksController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
@@ -17,23 +18,34 @@ import java.util.concurrent.ExecutionException;
 @RestController
 public class CryptopunksController {
 
+    public static final String PUNK_SALE_DETAIL_URL = "/cryptopunks";
+    public static final String PUNKS_FOR_SALE_LIST_URL = "/cryptopunks/sale";
     private static final Logger log = LoggerFactory.getLogger(CryptopunksController.class);
 
     @Autowired
     private CryptopunksService cryptopunksService;
 
-    @RequestMapping("/cryptopunks/sale")
+    @RequestMapping(PUNKS_FOR_SALE_LIST_URL)
     public List<BigInteger> getPunksForSale() throws ExecutionException, InterruptedException {
         return cryptopunksService.getPunksForSale();
     }
 
-    @RequestMapping("/cryptopunks")
+    @RequestMapping(PUNK_SALE_DETAIL_URL)
     public String getPunkDetailsForSale(@RequestParam(value = "id") BigInteger id)
     throws Exception {
         ObjectMapper mapper = new ObjectMapper();
 
-        Map <String, Object> map = cryptopunksService.getAllPunkDetails().get(id.toString());
-        map.put("sale", cryptopunksService.getPunkOfferedPrice(id));
-        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+        Map <String, Object> punkDetails = cryptopunksService.getAllPunkDetails().get(id.toString());
+
+        Map result = new HashMap();
+        if (punkDetails == null) {
+            result.put("gender", "Unknown");
+            result.put("accessories", "[]");
+        }
+        else {
+            result.putAll(punkDetails);
+        }
+        result.put("offerPrice", cryptopunksService.getPunkOfferedPrice(id));
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result);
     }
 }
